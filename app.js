0182f1436e4f43c68fb515bd70c198be98fe7af9
Lizdigital <lizetteclaireh@gmail.com>
2024-05-01 19:58:06 +0100

    Add accordion functionality - event handler

---
+++ app.js
@@ -0,0 +1,12 @@
+// Variable
+
+const accordion = document.getElementsByClassName('accordion-container');
+
+// for loop going through each element in the accordion (each accordion container)
+for (i = 0; i < accordion.length; i++) {
+    //'accordion[i]' represents the current accordion item element
+    accordion[i].addEventListener('click', function () {
+        //'this' refers to the element that triggered the event aka each specific accordion item container
+        this.classList.toggle('active');
+    })
+}
