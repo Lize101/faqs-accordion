c1c00121e8de4cea99a02b42dde09d393d6276bd
Lizdigital <lizetteclaireh@gmail.com>
2024-05-02 23:03:48 +0100

    Comment out console.log click tests

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const accordion = document.getElementsByClassName('accordion-container');
 
 // for loop going through each element in the accordion (each accordion container)
 for (i = 0; i < accordion.length; i++) {
-    console.log('clicked');
+    // console.log('clicked');
     //'accordion[i]' represents the current accordion item element
     accordion[i].addEventListener('click', function () {
         //'this' refers to the element that triggered the event aka each specific accordion item container
-        console.log('clicked');
+        // console.log('clicked');
         this.classList.toggle('active');
     })
 }
